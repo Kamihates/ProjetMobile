51ae5dfba8c51bc6285a563e630e0a01c26cd1a4
Elise <elizerosecampestrini@gmail.com>
2026-02-01 11:22:36 +0100

    visibilité des combinaisons

--- Assets/Scripts/Domino/Data/DominoInfos.cs
+++ Assets/Scripts/Domino/Data/DominoInfos.cs
@@ -1,9 +1,13 @@
 using System.Collections.Generic;
+using System.ComponentModel;
 using UnityEngine;
 
 [System.Serializable]
 public class DominoInfos 
 {
-    private List<RegionData> regions = new();
+    [SerializeField, NaughtyAttributes.ReadOnly] private List<RegionData> regions = new();
     public List<RegionData> Regions { get => regions; set => regions = value; }
+
+    // inspecteur
+
 }
